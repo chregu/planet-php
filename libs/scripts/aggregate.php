33441210e515223510bd62ccdc353d8bf5b18fdf
till <till@php.net>
2009-09-28 20:33:41 +0200

    made aggregate an executable script with, exit when param is missing

--- libs/scripts/aggregate.php
+++ libs/scripts/aggregate.php
@@ -1,6 +1,12 @@
+#!/usr/bin/env php
 <?php
-include_once('../../inc/config.inc.php');
-include_once('aggregator.php');
+include_once dirname(__FILE__) . '/../../inc/config.inc.php';
+include_once 'aggregator.php';
+
+if (!isset($argv[1]) || empty($argv[1])) {
+    echo "Usage ./aggregate.php foo\n";
+    exit(1);
+}
 
 $agg = new aggregator();
 $agg->aggregateAllBlogs($argv[1]);
