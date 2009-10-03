09aab6cd30dbb86306ec62a9ca4f8155ed9958ce
Till Klampaeckel <till@php.net>
2009-10-03 19:48:56 +0200

    router test

---
+++ public/router.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'Net/URL/Mapper.php';
+
+// Init Router
+$m = Net_URL_Mapper::getInstance();
+$m->connect('index', array('controller' => 'planet', 'action' => 'index'));
+$m->connect('index/:from', array('controller' => 'planet', 'action' => 'page'));
+
+$m->setScriptName('router.php');
+
+$match = $m->match($_SERVER['REQUEST_URI']);
+
+if ($match === null) {
+    die("MATCHED NULL");
+}
+var_dump($match);
