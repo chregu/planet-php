27ad105e30651f3aac9d6d6c8739dcf7385bfe79
Christian Weiske <cweiske@cweiske.de>
2009-10-03 22:50:58 +0200

    opml support

--- library/PlanetPEAR/Controller/Index.php
+++ library/PlanetPEAR/Controller/Index.php
@@ -18,4 +18,39 @@ class PlanetPEAR_Controller_Index extends PlanetPEAR_Controller_Base
 
         return $this->data;
     }
+
+    /**
+     * List OPML feed
+     *
+     * @return void
+     */
+    public function opml()
+    {
+        header('Content-Type: text/x-opml');
+        header('Content-Type: text/xml');
+        $project = PROJECT_NAME_HR;
+        $title   = PROJECT_NAME_HR . ' feed list';
+        $created = date('r');
+        echo <<<XML
+<?xml version="1.0" encoding="utf-8" ?>
+<opml version="2.0">
+ <head>
+  <title>{$title}</title>
+  <dateCreated>{$created}</dateCreated>
+ </head>
+ <body>
+XML;
+        foreach ($this->planet->getBlogs('default', true) as $data) {
+            echo '<outline type="rss" text="'
+                . htmlspecialchars($data['title'])
+                . '" xmlUrl="'
+                . htmlspecialchars($data['link'])
+                . '" />' . "\n";
+        }
+        echo <<< XML
+ </body>
+</opml>
+XML;
+        exit(0);
+    }
 }
