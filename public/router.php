ef10d6866259483e64d0d6731faba75a581d91a6
Till Klampaeckel <till@php.net>
2009-10-03 20:40:39 +0200

    added config, added controller/action bs

--- public/router.php
+++ public/router.php
@@ -1,17 +1,35 @@
 <?php
-require_once 'Net/URL/Mapper.php';
+// Get configuration
+if (!include dirname(__FILE__) . '/../inc/config.inc.php') {
+    die("No conf.");
+}
 
 // Init Router
-$m = Net_URL_Mapper::getInstance();
-$m->connect('index', array('controller' => 'planet', 'action' => 'index'));
-$m->connect('index/:from', array('controller' => 'planet', 'action' => 'page'));
+try {
+    $m = Net_URL_Mapper::getInstance();
+    $m->connect('index', array('controller' => 'index', 'action' => 'index'));
+    $m->connect('index/:from', array('controller' => 'index', 'action' => 'page'));
 
-#$m->setScriptName('router.php');
-#var_dump($_SERVER['REQUEST_URI']); exit;
+    #$m->setScriptName('router.php');
+    #var_dump($_SERVER['REQUEST_URI']); exit;
 
-$match = $m->match($_SERVER['REQUEST_URI']);
+    $match = $m->match($_SERVER['REQUEST_URI']);
 
-if ($match === null) {
-    die("MATCHED NULL");
+    if ($match === null) {
+        $match = array('controller' => 'planet', 'action' => 'index');
+    }
+} catch (Net_URL_Mapper_Exception $e) {
+    die("Something went wrong.");
 }
-var_dump($match);
+
+$controller = 'PlanetPEAR_Controller_' . ucfirst(strtolower($match['controller']));
+
+$controllerObj = new $controller;
+
+if (!isset($match['from'])) {
+    $data = call_user_func(array($controller, $match['action']);
+} else {
+    $data = call_user_func_array(array($controller, $match['action']), array($match['from']));
+}
+
+var_dump($data);
