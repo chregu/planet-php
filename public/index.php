411ba8e55bcd90b81dd56cb0577810de94c83205
Till Klampaeckel <till@php.net>
2009-10-03 19:35:14 +0200

    FIXME's

--- public/index.php
+++ public/index.php
@@ -11,15 +11,17 @@
 //
 // : sitemap.php,v 1.5 2002/03/27 07:59:05 chregu Exp $
 //apd_set_session_trace(35);
-/*if ($_SERVER['REMOTE_ADDR'] != "80.218.7.144") {
-        header("HTTP/1.1 503 Service Temporarily Unavailable");
+/*
+if ($_SERVER['REMOTE_ADDR'] != "80.218.7.144") {
+    header("HTTP/1.1 503 Service Temporarily Unavailable");
 	print "<h1>503 Service Temporarily Unavailable</h1>";
-        print "Software Maintenance. Please come back in a few minutes";
-die();
-}*/
-error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
+    print "Software Maintenance. Please come back in a few minutes";
+    die();
+}
+*/
+error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING); // FIXME
 
-ini_set("display_errors", 1);
+ini_set("display_errors", 1); // FIXME: remove
 
 if (!include dirname(__FILE__) . '/../inc/config.inc.php') {
     die("No conf.");
