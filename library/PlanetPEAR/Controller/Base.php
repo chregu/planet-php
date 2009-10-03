c1867cae82fbdaf68b8320cbf1bc5f998550998d
Till Klampaeckel <till@php.net>
2009-10-03 20:41:19 +0200

    base controller and index controller classes

---
+++ library/PlanetPEAR/Controller/Base.php
@@ -0,0 +1,8 @@
+<?php
+abstract class PlanetPEAR_Controller_Base
+{
+    public function __construct()
+    {
+        // maybe init global stuff here
+    }
+}
---
+++ library/PlanetPEAR/Controller/Index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author Till Klampaeckel <till@php.net>
+ */
+class PlanetPEAR_Controller_Index extends PlanetPEAR_Controller_Base
+{
+    public function index()
+    {
+        $this->page(0);
+    }
+
+    public function page($from)
+    {
+        $planet = new PlanetPEAR;
+        return $planet->getEntries('default', $from);
+    }
+}
